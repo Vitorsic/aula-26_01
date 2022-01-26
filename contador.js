48f93b8a502d8dc8a9c0c3e664db04e74acfe299
Vitor <vitorsic1@gmail.com>
2022-01-26 14:20:13 -0300

    adicionei o Impar

--- contador.js
+++ contador.js
@@ -4,10 +4,11 @@ for(let contador=0; contador<numeroAlunos;contador++){
 
     if (contador == 0){
         console.log("O número é Zero")
-    }
-
-    if(contador %2==0){
+    } else if(contador %2==0){
 
         console.log('O numero ' + contador + ' é PAR')
+    } else if(contador %2 == 1){
+        console.log('O número ' + contador + ' é IMPAR')
+
     }
 }
